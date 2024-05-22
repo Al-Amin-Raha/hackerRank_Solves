912ded54124761f8a30c6bae77b13523eb10c34e
Raha <md.alaminraha@gmail.com>
2024-05-22 18:07:42 +0600

    Update any or all.py

--- any or all.py
+++ any or all.py
@@ -1,10 +1,4 @@
-# Read input
 n = int(input())
 nums = input().split()
-conditions = all(int(i) > 0 for i in nums) and any(int(i) == 5 for i in nums)
+conditions = all(int(i) > 0 for i in nums) and any(i == i[::-1] for i in nums)
 print(conditions)
-    
-    
-# all_positive = all(int(x) > 0 for x in arr)
-# any_palindrome = any(x == x[::-1] for x in arr)
-# print(all_positive and any_palindrome)
