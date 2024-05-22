a4bd8b587a7321dd63fb731cbb73a4ad771485f8
Raha <md.alaminraha@gmail.com>
2024-05-22 18:08:53 +0600

    zipped.py

--- zipped.py
+++ zipped.py
@@ -1,7 +1,6 @@
 n, x = map(int, input().split())
 marks = [list(map(float, input().split())) for _ in range(x)]
-print (marks)
-# transposed_marks = zip(*marks)
-# average_scores = [sum(scores) / x for scores in transposed_marks]
-# for score in average_scores:
-#     print(score)
+total_marks = zip(*marks)
+avg_marks = [sum(scores) / x for scores in total_marks]
+for num in avg_marks:
+    print(num)
